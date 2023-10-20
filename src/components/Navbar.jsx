7637a94f04d78802afad327829f969b663d31df0
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-10-20 19:47:29 +0545

    opcty

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <div className='w-full h-[100px] m-auto z-10 opacity-100 fixed'>
 
-            <div className=" mx-auto p-5 md:px-6  flex justify-between items-center md:max-w-[1340px]  dark:bg-slate-800 drop-shadow-lg">
+            <div className=" mx-auto p-5 md:px-6  flex justify-between items-center md:max-w-[1340px] opacity-100 dark:bg-slate-800 drop-shadow-lg">
                 <div className="flex items-center">
                     {/*<img src={logo} alt="logo" className="sm:ml-10 ss:ml-10 md:ml-3 opacity-[55%} w-full h-[90px] py-1" /> */}
                     <Link to="/about">
