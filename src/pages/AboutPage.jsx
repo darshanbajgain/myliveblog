fbe022b10c95f498bd8eccb88f0f3987d5d9f9af
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-10-22 21:37:22 +0545

    build

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,20 @@
-
+import React, { useEffect } from 'react';
 import { Navbar, AboutContent, Footer } from "../components";
 
 const AboutPage = () => {
+    useEffect(() => {
+        // Set metadata for the About page
+        document.title = "About";
+        document.querySelector('meta[name="description"]').setAttribute('content', "HELLO, I AM DARSHAN BAJGAIN.");
+        document.querySelector('meta[property="og:image"]').setAttribute('content', "https://media.licdn.com/dms/image/D5603AQHiTbxjVI1AEQ/profile-displayphoto-shrink_800_800/0/1682674343890?e=1702512000&v=beta&t=W5e1A2B-E_AynuySpH0nzwKQbxUEY4ogC9swOA1K9ZU");
+    
+        // Clean up the metadata when the component unmounts
+        return () => {
+          document.title = "Darshan's Blog";
+          document.querySelector('meta[name="description"]').setAttribute('content', "Welcome to Dev.Darshan");
+          document.querySelector('meta[property="og:image"]').setAttribute('content', "../images/devdarshan.png");
+        };
+      }, [])
     return (
         <div className="home pb-2">
             <Navbar />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
 import Blogs from "../components/Blogs";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-
+import React, { useEffect } from 'react';
 
 const Homepage = ({ blog }) => {
+  useEffect(() => {
+    // Set metadata for the Homepage
+    document.title = "Darshan's Blog";
+    document.querySelector('meta[name="description"]').setAttribute('content', "Hi, Welcome to my blog, &lt;/DEV.DARSHAN&gt;");
+    document.querySelector('meta[property="og:url"]').setAttribute('content', "https://darshanblog.vercel.app/");
+    document.querySelector('meta[property="og:type"]').setAttribute('content', "website");
+    document.querySelector('meta[property="og:title"]').setAttribute('content', "Darshan's Blog");
+    document.querySelector('meta[property="og:description"]').setAttribute('content', "Hi, Welcome to my blog, &lt;/DEV.DARSHAN&gt;");
+    document.querySelector('meta[property="og:image"]').setAttribute('content', "https://darshanblog.vercel.app/images/devdarshan.png");
+    document.querySelector('meta[name="twitter:card"]').setAttribute('content', "summary_large_image");
+    document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "darshanblog.vercel.app");
+    document.querySelector('meta[property="twitter:url"]').setAttribute('content', "https://darshanblog.vercel.app/");
+    document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Darshan's Blog");
+    document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Hi, Welcome to my blog, &lt;/DEV.DARSHAN&gt;");
+    document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://darshanblog.vercel.app/images/devdarshan.png");
 
+    // Clean up the metadata when the component unmounts
+    return () => {
+      document.title = "Default Title";
+      document.querySelector('meta[name="description"]').setAttribute('content', "Default Description");
+      document.querySelector('meta[property="og:url"]').setAttribute('content', "Default URL");
+      document.querySelector('meta[property="og:type"]').setAttribute('content', "Default Type");
+      document.querySelector('meta[property="og:title"]').setAttribute('content', "Default Title");
+      document.querySelector('meta[property="og:description"]').setAttribute('content', "Default Description");
+      document.querySelector('meta[property="og:image"]').setAttribute('content', "Default Image URL");
+      document.querySelector('meta[name="twitter:card"]').setAttribute('content', "Default Card");
+      document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "Default Domain");
+      document.querySelector('meta[property="twitter:url"]').setAttribute('content', "Default URL");
+      document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Default Title");
+      document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Default Description");
+      document.querySelector('meta[name="twitter:image"]').setAttribute('content', "Default Image URL");
+    };
+  }, []);
 
-    return (
-        <div>
-            <Navbar />
-            <Blogs blog={blog} />
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Blogs blog={blog} />
+      <Footer />
+    </div>
+  );
+};
 
 export default Homepage;
