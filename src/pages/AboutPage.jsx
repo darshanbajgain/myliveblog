69b1c5ee2e307d0bd0801caf68168c978a494b2c
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-10-28 10:07:04 +0545

    1

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,13 +10,13 @@ const AboutPage = () => {
     document.querySelector('meta[property="og:type"]').setAttribute('content', "website");
     document.querySelector('meta[property="og:title"]').setAttribute('content', "Darshan's Blog- About");
     document.querySelector('meta[property="og:description"]').setAttribute('content', "Welcome to my blog. I work as a Frontend Web Developer and specialize in HTML, CSS, Javascript, and React JS. Currently, I am expanding my skills to include backend technologies such as Java, and have experience working with databases like SQL and MongoDB.");
-    document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app../images/devdarshan.png");
+    document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
     document.querySelector('meta[name="twitter:card"]').setAttribute('content', "summary_large_image");
     document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "darshanblog.vercel.app");
     document.querySelector('meta[property="twitter:url"]').setAttribute('content', "https://blogdarshan.netlify.app/about");
     document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Darshan's Blog- About");
     document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Welcome to my blog. I work as a Frontend Web Developer and specialize in HTML, CSS, Javascript, and React JS. Currently, I am expanding my skills to include backend technologies such as Java, and have experience working with databases like SQL and MongoDB.");
-    document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app../images/devdarshan.png");
+    document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
 
     // Clean up the metadata when the component unmounts
     return () => {
@@ -26,13 +26,13 @@ const AboutPage = () => {
       document.querySelector('meta[property="og:type"]').setAttribute('content', "website");
       document.querySelector('meta[property="og:title"]').setAttribute('content', "Darshan's Blog");
       document.querySelector('meta[property="og:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-      document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app../images/devdarshan.png");
+      document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
       document.querySelector('meta[name="twitter:card"]').setAttribute('content', "summary_large_image");
       document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "blogdarshan.netlify.app");
       document.querySelector('meta[property="twitter:url"]').setAttribute('content', "https://blogdarshan.netlify.app/");
       document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Darshan's Blog");
       document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-      document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app../images/devdarshan.png"); 
+      document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png"); 
     };
   }, []);
 
