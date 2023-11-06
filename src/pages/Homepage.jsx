733754d1b4310ecd6594d29e478193ae367b3edc
Darshan Bajgain <darshanbajgain90@gmail.com>
2023-11-06 10:16:48 +0545

    metadata updated+

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,34 +8,22 @@ const Homepage = ({ blog }) => {
     // Set metadata for the Homepage
 
     document.title = "Darshan's Blog";
-    document.querySelector('meta[name="description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-    document.querySelector('meta[property="og:url"]').setAttribute('content', "https://blogdarshan.netlify.app/");
+    document.querySelector('meta[name="description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content.");
+    document.querySelector('meta[property="og:url"]').setAttribute('content', "blogdarshan.netlify.app");
     document.querySelector('meta[property="og:type"]').setAttribute('content', "website");
     document.querySelector('meta[property="og:title"]').setAttribute('content', "Darshan's Blog");
-    document.querySelector('meta[property="og:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
+    document.querySelector('meta[property="og:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content.");
     document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
     document.querySelector('meta[name="twitter:card"]').setAttribute('content', "summary_large_image");
     document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "blogdarshan.netlify.app");
-    document.querySelector('meta[property="twitter:url"]').setAttribute('content', "https://blogdarshan.netlify.app/");
+    document.querySelector('meta[property="twitter:url"]').setAttribute('content', "blogdarshan.netlify.app");
     document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Darshan's Blog");
-    document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
+    document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content.");
     document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
     
     // Clean up the metadata when the component unmounts
     return () => {
       document.title = "Darshan's Blog";
-      document.querySelector('meta[name="description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-      document.querySelector('meta[property="og:url"]').setAttribute('content', "https://blogdarshan.netlify.app/");
-      document.querySelector('meta[property="og:type"]').setAttribute('content', "website");
-      document.querySelector('meta[property="og:title"]').setAttribute('content', "Darshan's Blog");
-      document.querySelector('meta[property="og:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-      document.querySelector('meta[property="og:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
-      document.querySelector('meta[name="twitter:card"]').setAttribute('content', "summary_large_image");
-      document.querySelector('meta[property="twitter:domain"]').setAttribute('content', "blogdarshan.netlify.app");
-      document.querySelector('meta[property="twitter:url"]').setAttribute('content', "https://blogdarshan.netlify.app/");
-      document.querySelector('meta[name="twitter:title"]').setAttribute('content', "Darshan's Blog");
-      document.querySelector('meta[name="twitter:description"]').setAttribute('content', "Hi, Welcome to &lt;/DEV.DARSHAN&gt;, the blog of Darshan Bajgain. Explore a world of insightful articles covering technology,and more. Stay informed, inspired, and up-to-date with our expertly crafted content. ");
-      document.querySelector('meta[name="twitter:image"]').setAttribute('content', "https://blogdarshan.netlify.app/images/devdarshan.png");
     };
   }, []);
 
